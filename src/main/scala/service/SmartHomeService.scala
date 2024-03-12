7c973e16aa23e3454df0c2e44ebddb9c3a7701c3
davidleacock <dave.leacock@gmail.com>
2024-03-12 13:56:28 -0700

    Add new commands and events

--- src/main/scala/service/SmartHomeService.scala
+++ src/main/scala/service/SmartHomeService.scala
@@ -12,11 +12,15 @@ trait SmartHomeService[F[_]] {
 object SmartHomeService {
   sealed trait Command
   case class AddDevice(homeId: UUID, device: Device) extends Command
+  case class UpdateDevice(homeId: UUID, deviceId: UUID, newValue: Int) extends Command // TODO generalize the newValue
+  case class GetSmartHome(homeId: UUID) extends Command
 
   sealed trait Event
   case class DeviceAdded(homeId: UUID, device: Device) extends Event
+  case class DeviceUpdated(homeId: UUID, device: Device) extends Event
 
   sealed trait SmartHomeResult
   case object Success extends SmartHomeResult
   case object Failure extends SmartHomeResult
+  case class Result(payload: String) extends SmartHomeResult
 }
