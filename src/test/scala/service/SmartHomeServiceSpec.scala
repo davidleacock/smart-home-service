4bb0cc724da0e982218815ab03628f50e1389146
davidleacock <dave.leacock@gmail.com>
2024-03-14 12:04:39 -0700

    add more tests

--- src/test/scala/service/SmartHomeServiceSpec.scala
+++ src/test/scala/service/SmartHomeServiceSpec.scala
@@ -2,30 +2,122 @@ package service
 
 import cats.effect.testing.scalatest.AsyncIOSpec
 import domain.Thermostat
-import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AsyncWordSpec
 import repo.impl.InMemorySmartHomeEventRepo
-import service.SmartHomeService.{AddDevice, Success}
+import service.SmartHomeService.{AddDevice, DeviceUpdated, GetSmartHome, Result, Success, UpdateDevice}
 
 import java.util.UUID
 
-class SmartHomeServiceSpec extends AsyncFlatSpec with AsyncIOSpec with Matchers {
+class SmartHomeServiceSpec
+    extends AsyncWordSpec
+    with AsyncIOSpec
+    with Matchers {
 
+  "SmartHomeService" should {
 
-  "SmartHomeService" should "process command and persist event" in {
+    "process an AddDevice command by adding new device" in {
+      val test = for {
+        repo <- InMemorySmartHomeEventRepo.create
+        service = new SmartHomeServiceImpl(repo)
+        homeId = UUID.randomUUID()
+        deviceId = UUID.randomUUID()
 
-    val test = for {
-      repo <- InMemorySmartHomeEventRepo.create
-      service = new SmartHomeServiceImpl(repo)
-      homeId = UUID.randomUUID()
+        addResult <- service.processCommand(
+          homeId,
+          AddDevice(homeId, Thermostat(deviceId, value = 1))
+        )
+        persistedEvents <- repo.retrieveEvents(homeId)
+      } yield {
+        addResult shouldBe Success
+        persistedEvents should have size 1
+      }
 
-      addResult <- service.processCommand(homeId, AddDevice(homeId, Thermostat(homeId, value = 1)))
-      persistedEvents <- repo.retrieveEvents(homeId)
-    } yield {
-      addResult shouldBe Success
-      persistedEvents should have size 1
+      test.assertNoException
     }
 
-    test.assertNoException
+    "process an UpdateDevice command by updating an existing device" in {
+      val test = for {
+        repo <- InMemorySmartHomeEventRepo.create
+        service = new SmartHomeServiceImpl(repo)
+        homeId = UUID.randomUUID()
+        deviceId = UUID.randomUUID()
+
+        _ <- service.processCommand(
+          homeId,
+          AddDevice(homeId, Thermostat(deviceId, value = 1))
+        )
+        updateResult <- service.processCommand(
+          homeId,
+          UpdateDevice(homeId, deviceId, 10)
+        )
+        persistedEvents <- repo.retrieveEvents(homeId)
+      } yield {
+        updateResult shouldBe Success
+        persistedEvents should have size 2
+        persistedEvents.tail.head match {
+          case DeviceUpdated(_, device) => device.currValue shouldBe 10
+          case _ => fail("most recent event isn't DeviceUpdated")
+        }
+      }
+
+      test.assertNoException
+    }
+
+    "process an multiple UpdateDevice command by updating an existing device" in {
+      val test = for {
+        repo <- InMemorySmartHomeEventRepo.create
+        service = new SmartHomeServiceImpl(repo)
+        homeId = UUID.randomUUID()
+        deviceId = UUID.randomUUID()
+
+        _ <- service.processCommand(
+          homeId,
+          AddDevice(homeId, Thermostat(deviceId, value = 1))
+        )
+        _ <- service.processCommand(
+          homeId,
+          UpdateDevice(homeId, deviceId, 10)
+        )
+        updateResult <- service.processCommand(
+          homeId,
+          UpdateDevice(homeId, deviceId, 20)
+        )
+        persistedEvents <- repo.retrieveEvents(homeId)
+      } yield {
+        updateResult shouldBe Success
+        persistedEvents should have size 3
+        persistedEvents.last match {
+          case DeviceUpdated(_, device) => device.currValue shouldBe 20
+          case _ => fail("most recent event isn't DeviceUpdated")
+        }
+      }
+
+      test.assertNoException
+    }
+
+    "process an GetSmartHome command by returning existing SmartHome state" in {
+      val test = for {
+        repo <- InMemorySmartHomeEventRepo.create
+        service = new SmartHomeServiceImpl(repo)
+        homeId = UUID.randomUUID()
+        deviceId = UUID.randomUUID()
+
+        _ <- service.processCommand(
+          homeId,
+          AddDevice(homeId, Thermostat(deviceId, value = 1))
+        )
+        _ <- service.processCommand(
+          homeId,
+          UpdateDevice(homeId, deviceId, 10)
+        )
+        result <- service.processCommand(homeId, GetSmartHome(homeId))
+      } yield {
+        result shouldBe Result(s"Result from $homeId: List(Thermostat($deviceId,Thermostat,10))")
+
+      }
+
+      test.assertNoException
+    }
   }
 }
