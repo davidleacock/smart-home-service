4e4d1cf24c4a4f80c5829f62bd57e4ae76380e82
davidleacock <dave.leacock@gmail.com>
2024-03-15 13:13:16 -0700

    update Device, add DVT as a way to wrap the value type. Add MotionDetector

--- src/main/scala/domain/Device.scala
+++ src/main/scala/domain/Device.scala
@@ -2,45 +2,54 @@ package domain
 
 import java.util.UUID
 
-// The current value needs to be more generic
+sealed trait DeviceValueType {
+  def value: Any
+}
+case class IntDVT(value: Int) extends DeviceValueType
+case class StringDVT(value: String) extends DeviceValueType
 
-trait Device {
-  def id: UUID
-  def deviceType: DeviceType
-  def updated(newValue: Int): Device
-  def currValue: Int
+object DeviceValueTypeImplicits {
+  implicit class DeviceValueTypeOps(dvt: DeviceValueType) {
+    def unwrapped: Any = dvt match {
+      case IntDVT(value)    => value
+      case StringDVT(value) => value
+    }
+  }
 }
 
-trait DeviceType
-case object Thermostat extends DeviceType
-case object MotionDetector extends DeviceType
+trait Device {
+  val id: UUID
+  val currValue: DeviceValueType
+  def applyUpdate(newValue: DeviceValueType): Either[DeviceError, Device]
+}
 
-// TODO how does Device validation work?
+case class DeviceError(reason: String)
+
 case class Thermostat(
   id: UUID,
-  deviceType: DeviceType = Thermostat,
   value: Int)
     extends Device {
 
-  override def updated(newValue: Int): Device =
-    // ! arbitrary error value - replace
-    if (!newValue.equals(999)) {
-      this.copy(value = newValue)
-    } else this
+  override def applyUpdate(newValue: DeviceValueType): Either[DeviceError, Device] =
+    // ! arbitrary error value - replace with rule validation
+    newValue match {
+      case IntDVT(v) if v != 999 => Right(copy(value = v))
+      case _                     => Left(DeviceError("Invalid value for Thermostat"))
+    }
 
-  override def currValue: Int = this.value
+  override val currValue: DeviceValueType = IntDVT(this.value)
 }
 
 case class MotionDetector(
   id: UUID,
-  deviceType: DeviceType = MotionDetector,
-  value: Int)
+  value: String)
     extends Device {
-  override def updated(newValue: Int): Device =
-    // ! arbitrary error value - replace
-    if (!newValue.equals(888)) {
-      this.copy(value = newValue)
-    } else this
+  override def applyUpdate(newValue: DeviceValueType): Either[DeviceError, Device] =
+    // ! arbitrary error value - replace with rule validation
+    newValue match {
+      case StringDVT(v) if v != "error" => Right(copy(value = v))
+      case _                            => Left(DeviceError("Invalid value for MotionDetector"))
+    }
 
-  override def currValue: Int = this.value
+  override val currValue: DeviceValueType = StringDVT(this.value)
 }
