2135b7e028fd0f6ced6cbf3a5b26263ac5e4219c
davidleacock <dave.leacock@gmail.com>
2024-03-29 12:45:35 -0700

    create TemperatureSettings for SmartHome

--- src/main/scala/domain/SmartHome.scala
+++ src/main/scala/domain/SmartHome.scala
@@ -2,4 +2,10 @@ package domain
 
 import java.util.UUID
 
-case class SmartHome(homeId: UUID, devices: List[Device], currentTemperature: Option[Int])
+case class SmartHome(
+  homeId: UUID,
+  devices: List[Device],
+  currentTemperature: Option[Int],
+  temperatureSettings: Option[TemperatureSettings])
+
+case class TemperatureSettings(minTemp: Int, maxTemp: Int)
