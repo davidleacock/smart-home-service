c9b24a44c09bbf8381627cd21eb7b4c1abcee3bd
davidleacock <dave.leacock@gmail.com>
2024-03-31 13:49:47 -0700

    Use Streaming solution for querying from repo

--- src/main/scala/repo/SmartHomeEventRepository.scala
+++ src/main/scala/repo/SmartHomeEventRepository.scala
@@ -1,10 +1,11 @@
 package repo
 
 import service.SmartHomeService.Event
+import fs2.Stream
 
 import java.util.UUID
 
 trait SmartHomeEventRepository[F[_]] {
   def persistEvent(homeId: UUID, event: Event): F[Unit]
-  def retrieveEvents(homeId: UUID): F[List[Event]]
+  def retrieveEvents(homeId: UUID): Stream[F, Event]
 }
