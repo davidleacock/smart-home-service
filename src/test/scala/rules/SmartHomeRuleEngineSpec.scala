a36f897d989564be3e5a35afbfc65acf5d515709
davidleacock <dave.leacock@gmail.com>
2024-04-20 11:49:51 -0700

    add tests for SmartHomeRuleEngineSpec

--- src/test/scala/rules/SmartHomeRuleEngineSpec.scala
+++ src/test/scala/rules/SmartHomeRuleEngineSpec.scala
@@ -1,3 +1,64 @@
 package rules
 
-class SmartHomeRuleEngineSpec {}
+import cats.data.Validated
+import domain.MotionState.MotionNotDetected
+import domain.{IntDVT, SmartHome, TemperatureSettings, Thermostat}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import service.SmartHomeService.{AddDevice, DeviceAdded, DeviceUpdated, EventSuccess, SetTemperatureSettings, TemperatureSettingsSet, UpdateDevice}
+
+import java.util.UUID
+
+class SmartHomeRuleEngineSpec extends AnyWordSpec with Matchers {
+
+  val ruleEngine = new SmartHomeRuleEngine
+  val homeId: UUID = UUID.randomUUID()
+
+  "SmartHomeRuleEngine" should {
+
+    "validate AddDevice command" in {
+      val device = Thermostat(UUID.randomUUID(), 25)
+      val command = AddDevice(device)
+      val state = SmartHome(homeId, List.empty, None, None, MotionNotDetected)
+
+      val result = ruleEngine.validate(command, state)
+      result shouldBe Validated.valid(EventSuccess(DeviceAdded(device)))
+    }
+
+    "validate UpdateDevice command when device exists" in {
+      val deviceId = UUID.randomUUID()
+      val device = Thermostat(deviceId, 25)
+      val command = UpdateDevice(deviceId, IntDVT(30))
+      val state = SmartHome(homeId, List(device), None, None, MotionNotDetected)
+
+      val result = ruleEngine.validate(command, state)
+      result shouldBe Validated.valid(EventSuccess(DeviceUpdated(device.copy(value = 30))))
+    }
+
+    "fail to validate UpdateDevice command when device doesn't exist" in {
+      val deviceId = UUID.randomUUID()
+      val command = UpdateDevice(deviceId, IntDVT(30))
+      val state = SmartHome(homeId, List.empty, None, None, MotionNotDetected)
+
+      val result = ruleEngine.validate(command, state)
+      result shouldBe Validated.invalidNec(s"Device $deviceId not found.")
+    }
+
+    "validate SetTemperature command within valid range" in {
+      val command = SetTemperatureSettings(10, 20)
+      val state = SmartHome(homeId, List.empty, None, None, MotionNotDetected)
+
+      val result = ruleEngine.validate(command, state)
+      result shouldBe Validated.valid(EventSuccess(TemperatureSettingsSet(TemperatureSettings(10, 20))))
+
+    }
+
+    "fail to validate SetTemperature command outside of valid range" in {
+      val command = SetTemperatureSettings(30, 10)
+      val state = SmartHome(homeId, List.empty, None, None, MotionNotDetected)
+
+      val result = ruleEngine.validate(command, state)
+      result shouldBe Validated.invalidNec("Invalid temperature settings: [30, 10] must be between 0 and 50.")
+    }
+  }
+}
