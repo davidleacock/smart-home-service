310217118e2e3a0cc403139dea1782960ccb09f2
davidleacock <dave.leacock@gmail.com>
2024-07-13 13:03:56 -0700

    Refactor HttpServer and rename

--- src/main/scala/http/SmartHomeHttpServer.scala
+++ src/main/scala/http/SmartHomeHttpServer.scala
@@ -1,32 +1,19 @@
 package http
 
-import cats.effect.{ExitCode, IO, Resource}
+import cats.effect.{IO, Resource}
 import com.comcast.ip4s._
 import org.http4s.Method.GET
-import org.http4s.{HttpRoutes, _}
 import org.http4s.dsl.impl.UUIDVar
 import org.http4s.dsl.io._
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.implicits._
 import org.http4s.server.middleware.Logger
 import org.http4s.server.{Router, Server}
+import org.http4s.{HttpRoutes, _}
 import service.SmartHomeService
 import service.SmartHomeService.{Failure, GetSmartHome, ResponseResult, Success}
 
-// ! TODO - test this
-class HttpServer(serverResource: Resource[IO, Server]) {
-
-  // ! TODO - handle errors
-  def start: IO[Unit] =
-    serverResource
-      .use { server =>
-        IO(println(s"Server up and running ${server.address}")) *> IO.never
-      }
-      .as(ExitCode.Success)
-}
-
-object HttpServer {
-
+object SmartHomeHttpServer {
   // ! TODO - Add more commands/routes, break down the failures and responses even further
   def createServerResources(smartHomeService: SmartHomeService[IO]): Resource[IO, Server] = {
     val routes = HttpRoutes.of[IO] { case GET -> Root / "home" / UUIDVar(homeId) =>
